2e6f506daee965e62c8d2e14f61874c5643e02e4
Andaç Erdoğmuş <andac.erdogmus@etiya.com>
2022-08-17 17:43:04 +0300

    env edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: "https://gpsvqsdkdhyifhkgthzm.supabase.co",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwc3Zxc2RrZGh5aWZoa2d0aHptIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAxMjgxNDEsImV4cCI6MTk3NTcwNDE0MX0.wk0m27qurPuLTRuPsPAVmERLCVxtJNbxTj5xBYGSuLY",
 };
