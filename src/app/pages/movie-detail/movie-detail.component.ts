fda0fea0aea7af349f6374c711601bb92e069710
eylul <eylulzan1999@gmail.com>
2022-08-23 13:47:39 +0300

    second commit

--- src/app/pages/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { take } from "rxjs";
-import { Cast } from "../../models/cast.interface";
-import { Movie } from "../../models/movie.interface";
-import { MovieService } from "../../services/movie.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { Cast } from '../../models/cast.interface';
+import { Movie } from '../../models/movie.interface';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
-  selector: "app-movie-detail",
-  templateUrl: "./movie-detail.component.html",
-  styleUrls: ["./movie-detail.component.scss"],
+  selector: 'app-movie-detail',
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.scss'],
 })
 export class MovieDetailComponent implements OnInit {
   movie: Movie;
@@ -23,13 +23,13 @@ export class MovieDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
-      this.getMovieById(params["id"]);
-      this.getMovieVideo(params["id"]);
-      this.getCast(params["id"]);
+      this.getMovieById(params['id']);
+      this.getMovieVideo(params['id']);
+      this.getCast(params['id']);
 
       // JavaScript DOM Manipülasyonu
-      const tag = document.createElement("script");
-      tag.src = "https://www.youtube.com/iframe_api";
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
       document.body.appendChild(tag);
     });
   }
@@ -50,7 +50,7 @@ export class MovieDetailComponent implements OnInit {
       .subscribe((response: any) => {
         if (response.results.length) {
           this.video = response.results.filter(
-            (video) => video.type === "Trailer"
+            (video) => video.type === 'Trailer'
           )[0];
           this.loading = false;
         }
