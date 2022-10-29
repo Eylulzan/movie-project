7891e68d1e241748cc0ebd6a145df05dfc3ca0d2
eylul <eylulzan1999@gmail.com>
2022-10-29 13:05:35 +0300

    four commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: "https://gpsvqsdkdhyifhkgthzm.supabase.co",
+  supabaseUrl: "https://btlffqjjaphlpnvfpjrb.supabase.co",
   supabaseKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwc3Zxc2RrZGh5aWZoa2d0aHptIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAxMjgxNDEsImV4cCI6MTk3NTcwNDE0MX0.wk0m27qurPuLTRuPsPAVmERLCVxtJNbxTj5xBYGSuLY",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0bGZmcWpqYXBobHBudmZwanJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAxMjgxNDcsImV4cCI6MTk3NTcwNDE0N30.cZ3UBCnruFY3TtBxIgQX2vr3wjM7n21MDxDR9g8QBh4",
 };
